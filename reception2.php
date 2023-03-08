02646ff0154edead9b976609e56e35c7129e0f7e
bstocker <boris.stocker@gmail.com>
2023-03-08 16:19:58 +0100

    Update reception2.php

--- reception2.php
+++ reception2.php
@@ -4,17 +4,17 @@
 
 
 <?php
+  
+$ANNEE = $_POST['annee'];
+  
 //Lister le contenu de la table FILMS
 
-  
-  
-  
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20407984_pacov', 'id20407984_pacova', 'KF{[\LwgIH6~#MJr');
 $base->exec("SET CHARACTER SET utf8");
 
 //2° - Préparation de requette et execution
-$retour = $base->query('SELECT * FROM movies;');
+$retour = $base->query('SELECT * FROM movies WHERE annee = $ANNEE;');
 
 //3° - Lecture du resultat de la requette
 while ($data = $retour->fetch()){
