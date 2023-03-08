fc1840dc797c40aac732fdf047a308808388e708
Stage <99012641+PacoV12@users.noreply.github.com>
2023-03-08 17:59:12 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,45 @@
+<?php if ($_SERVER['REQUEST_METHOD'] === 'GET'): ?>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Title</title>
+    </head>
+    <body>
+      <div>
+        <h1>Ajouter un film</h1>
+      </div>
+      <?php if (isset($_GET['e'])): ?>
+        <div>
+          <h3 style="color: red">Impossible d'ajouter un film</h3>
+        </div>
+      <?php endif ?>
+      <div>
+        <form method="post">
+          <label for="title">Titre</label>
+          <input type="text" name="title" id="title">
+          <label for="year">Année</label>
+          <input type="number" name="year" id="year">
+          <label for="genre">Genre</label>
+          <input type="text" name="genre" id="genre">
+          <button type="submit">Enregistrer</button>
+        </form>
+      </div>
+    </body>
+  </html>
+<?php else: ?>
+  <?php
+    require_once '../Database.php';
+
+    $title = $_POST['title'];
+    $year = $_POST['year'];
+    $genre = $_POST['genre'];
+    $db = new Database();
+
+    if ($db->insertMovie($title, $year, $genre)) {
+      header('Location: /');
+    } else {
+      header('Location: /add.php?e=1');
+    }
+  ?>
+<?php endif ?>
